1509ba56d988746ad0e93693559e118885c6b7af
Jira Pit <77648890+JiraPit@users.noreply.github.com>
2024-04-24 13:42:47 +0700

    Delete server/deploy.sh

--- server/deploy.sh
+++
@@ -1,5 +0,0 @@
-gcloud auth configure-docker asia-southeast1-docker.pkg.dev
-gcloud config set project cmkl-lambdafinder
-docker login -u _json_key --password-stdin asia-southeast1-docker.pkg.dev/ <cmkl-lambdafinder-e9e5e1e1144e.json
-docker build -t asia-southeast1-docker.pkg.dev/cmkl-lambdafinder/lambda-finder/lambda-finder .
-docker push asia-southeast1-docker.pkg.dev/cmkl-lambdafinder/lambda-finder/lambda-finder
